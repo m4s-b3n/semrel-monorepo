84d717503b6b2e91a1f1cba1ee07e24ad1b12d10
Marius Boden <marius.boden@xebia.com>
2026-03-10 18:33:34 +0000

    feat: add SayGoodbye to Alpha

--- src/Package.Alpha/Class1.cs
+++ src/Package.Alpha/Class1.cs
@@ -3,4 +3,6 @@ namespace Package.Alpha;
 public sealed class AlphaGreeter
 {
   public string SayHello(string name) => $"Hello {name} from Alpha";
+
+  public string SayGoodbye(string name) => $"Goodbye {name} from Alpha";
 }
