710bab1d901d7f2a4db8de5eb48c182386de129a
Marius Boden <marius.boden@xebia.com>
2026-03-11 08:42:45 +0000

    feat(beta,gamma): add SayGoodbye method

--- src/Package.Gamma/Class1.cs
+++ src/Package.Gamma/Class1.cs
@@ -3,4 +3,6 @@ namespace Package.Gamma;
 public sealed class GammaGreeter
 {
   public string SayHello(string name) => $"Hello {name} from Gamma";
+
+  public string SayGoodbye(string name) => $"Goodbye {name} from Gamma";
 }
